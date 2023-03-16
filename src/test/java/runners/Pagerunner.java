9cc980a0f824bdc525a08c1eef8b7b2d37c768b0
ASUS <ASUS@LAPTOP-1LL282FT>
2023-03-16 18:38:35 +0530

    BDD project

--- src/test/java/runners/Pagerunner.java
+++ src/test/java/runners/Pagerunner.java
@@ -6,8 +6,10 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 		features="src//test//resources//features//Items.feature",
 		glue={"stepDefs"},
+		monochrome = true,
+		dryRun = false,
 		plugin= {"pretty",
-				"html:target//Reports//HTMLReport.html"}
+				"html:target//reports//htmlreport"}
 		)
 
  public class Pagerunner extends AbstractTestNGCucumberTests {
